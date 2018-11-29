244d07f1b1cf2fff6b2ee7741a9bca6dac25c5e3
Alexander Berntsson <alexande.berntsson@gmail.com>
2018-11-29 22:55:04 +0100

    fixed bug where it created 2 canvas's

--- game.js
+++ game.js
@@ -72,7 +72,6 @@ var gameArea = {
 	canvas: document.body.getElementsByTagName("canvas")[0],
 	start: function() {
 		this.context = this.canvas.getContext("2d");
-		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 	},
 	clear: function() {
 		this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
